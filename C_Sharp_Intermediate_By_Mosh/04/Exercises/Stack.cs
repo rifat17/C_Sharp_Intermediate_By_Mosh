abe201762c35e0b636c6ae04b07b584c69161882
rifat17 <a.hasib.rifat@gmail.com>
2020-03-12 16:04:46 +0600

    Solved exercise from 04
    solved exercise from 'Inharitance - Second pillar of OOP'.

---
+++ C_Sharp_Intermediate_By_Mosh/04/Exercises/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+namespace C_Sharp_Intermediate_By_Mosh.Exercises
+{
+    public class Stack
+    {
+        private ArrayList list;
+
+        public Stack()
+        {
+            list = new ArrayList();
+        }
+
+        public void Push(object item)
+        {
+            if (item == null)
+                throw new InvalidOperationException("Cannot push NULL");
+            list.Add(item);
+        }
+
+        public object Pop()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Stack is empty");
+            int listSize = list.Count;
+            object obj = list[listSize - 1];
+            list.Remove(obj);
+            return obj;
+        }
+
+        public void Clear()
+        {
+            list = new ArrayList();
+        }
+    }
+}
